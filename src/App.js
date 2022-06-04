c23893aebd46d971787a535abe3a60cb1671796e
Backheart-js <binhnguyen29122001@gmail.com>
2022-06-04 16:01:04 +0700

    Setup Route and Layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { publicRoutes } from "~/routes/index";
+import { DefaultLayout } from "~/conponents/Layout";
+import { Fragment } from "react";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>React app</h1>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route,index) => {
+            const Page = route.component;
+            let Layout = DefaultLayout;
+            if(route.layout) {
+              Layout = route.layout;
+            } 
+            else if(route.layout === null) {
+              Layout = Fragment;
+            }
+
+            return <Route key={index} path={route.path} element={
+              <Layout>
+                <Page />
+              </Layout>
+            } />
+          })}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
