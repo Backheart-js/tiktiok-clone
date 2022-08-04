92363b747092e0ad92f89dbd5a3c80d92257816b
Backheart-js <binhnguyen29122001@gmail.com>
2022-08-04 21:58:35 +0700

    Hoan thanh bài 7 header

--- src/conponents/Image/index.js
+++ src/conponents/Image/index.js
@@ -1,10 +1,16 @@
-import React, { forwardRef } from 'react';
+import React, { useState, forwardRef } from 'react';
 import img from '~/assets/image';
 
-const Image = forwardRef(({ className, src, ...props }, ref) => {
-    
+const Image = forwardRef(({ className, src, fallback: customFallback = src.noImage, ...props }, ref) => {
+  const [fallback, setFallback] = useState('');
+
+  const handleError = () => {
+    setFallback(customFallback);
+  }
+
+
   return (
-    <img ref={ref} className = {className} src = {src} {...props} />
+    <img ref={ref} className = {className} src = {fallback || src} {...props} onError={handleError} />
   )
 })
 
